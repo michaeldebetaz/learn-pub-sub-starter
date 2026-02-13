4200da092197fe401686a062dd40329b5b92f90c
michaeldebetaz <netarion@outlook.com>
2026-02-13 19:19:33 +0100

    feat: CH.2 L4: Connect done

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const URL = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	slog.Info("Starting Peril server...")
+
+	conn, err := amqp.Dial(URL)
+	if err != nil {
+		err := fmt.Errorf("Failed to connect to AMQP: %w", err)
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	slog.Info("Successfully connected to AMQP")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
